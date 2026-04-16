65a8be2ebea26e5db033b71ba697d62dcbce943b
jeloco40-collab <>
2026-04-16 15:49:03 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,9 @@
+item_name = str("Bread")
+item_price = float(4.52)
+items_in_stock = int(230)
+
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
