e0136e759d6cf38c234bc1e42478cb49aba42d62
jeloco40-collab <>
2026-04-16 15:49:10 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,8 @@
+item_name = str("Soda")
+item_price = float(6.99)
+purchase_quantity = int(5)
+total_cost = item_price * purchase_quantity
+
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
